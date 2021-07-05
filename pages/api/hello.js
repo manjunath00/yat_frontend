0076375286ac7a90bb61044f3952ef49f10638f7
manjunath00 <kadrimanjunath56@gmail.com>
2021-07-06 01:43:05 +0530

    Added axios client

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.headers.post["Content-Type"] = "applicaton/json";
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log("Request logged", request);
+    const token = localStorage.getItem("token");
+
+    request.headers.authorization = `Bearer ${token}`;
+
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log("Response logged", response.data);
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error.response);
+  }
+);
+
+export default axios;
