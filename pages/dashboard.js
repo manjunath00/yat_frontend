6eb008e8abdb5c129807145a615f2bc0cce18325
manjunath00 <kadrimanjunath56@gmail.com>
2021-07-06 01:44:05 +0530

    Added new task & toggle task functionality

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,18 +1,30 @@
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Container,
-  Navbar,
-  NavbarBrand,
-} from "reactstrap";
+import { Input, Container } from "reactstrap";
 import React, { useState, useEffect } from "react";
+
+import yatApi from "../pages/api/hello";
+import Router from "next/router";
 
 const Dashboard = (props) => {
   const { todos = [] } = props;
   const [tasks, setTasks] = useState(todos);
+  const [allTasks, setAllTasks] = useState(todos);
+
+  const getTasks = async () => {
+    try {
+      const response = await yatApi.get("/api/task");
+      console.log("Not Executed");
+      setTasks(response.data);
+      setAllTasks(response.data);
+    } catch (error) {
+      console.log("Error ", error.message);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(function () {
+      return getTasks();
+    }, 500);
+  }, []);
 
   const onStatusChange = (e, id) => {
     const isChecked = e.target.checked;
@@ -28,13 +40,42 @@ const Dashboard = (props) => {
     setTasks(newTasks);
   };
 
+  const onToggleStatus = (status = true) => {
+    const completedTasks = allTasks.filter((item) => item.completed === status);
+    setTasks(completedTasks);
+  };
+
+  const onDeleteTask = async (id) => {
+    const response = await yatApi.delete(`/api/task/${id}`);
+    return getTasks();
+  };
+
+  const onToggleTask = async (id, completed) => {
+    const response = await yatApi.patch(`/api/task/${id}`, {
+      completed: !completed,
+    });
+    return getTasks();
+  };
+
+  const onLogout = () => {
+    localStorage.removeItem("token");
+    Router.push("/login");
+  };
+
   return (
     <Container>
-      <h3>Dashboard</h3>
-
+      <div className="flex">
+        <h3>Dashboard</h3>
+        <div>
+          <button onClick={() => onLogout()}>Logout</button>
+        </div>
+      </div>
       <div className="flex">
         <div className="col-3 sidebar">
-          <div className="flex sidebar-item">
+          <div
+            className="flex sidebar-item"
+            onClick={() => onToggleStatus(true)}
+          >
             <h6>Completed</h6>
             <span>50</span>
           </div>
@@ -42,7 +83,10 @@ const Dashboard = (props) => {
             <h6>Favourites </h6>
             <span>50</span>
           </div>
-          <div className="flex sidebar-item">
+          <div
+            className="flex sidebar-item"
+            onClick={() => onToggleStatus(false)}
+          >
             <h6>Todo</h6>
             <span>50</span>
           </div>
@@ -50,17 +94,20 @@ const Dashboard = (props) => {
         <div className="col-9 main-bar">
           <h3>Todos</h3>
           <div>
-            {tasks.map(({ id, title, description, completed }) => (
-              <div key={id} className="todo flex-layout">
-                {" "}
+            {tasks.map(({ _id, name, description, completed }) => (
+              <div key={_id} className="todo flex-layout">
                 <Input
                   type="checkbox"
                   checked={completed}
-                  onChange={(e) => onStatusChange(e, id)}
-                />{" "}
-                <div className="todo-text">
-                  <span className="todo-text-primary">{title}</span>
-                  <div>{description}</div>
+                  onChange={() => onToggleTask(_id, completed)}
+                />
+                <div className="flex">
+                  <div className="todo-text">
+                    <div className="todo-text-primary">{name}</div>
+                    <div>{description}</div>
+                  </div>
+
+                  <div onClick={() => onDeleteTask(_id)}>Delete</div>
                 </div>
               </div>
             ))}
--- pages/new.js
+++ pages/new.js
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input, Container } from "reactstrap";
 
+import yatApi from "../pages/api/hello";
+
 const NewTask = () => {
   const [taskName, setTaskName] = useState("");
   const [description, setDescription] = useState("");
   const [completed, setCompleted] = useState(false);
 
+  const asyncNewTask = async (data) => {
+    const response = await yatApi.post("/api/task", data);
+  };
+
   const onFormSubmit = (e) => {
     e.preventDefault();
     const task = {
-      taskName,
+      name: taskName,
       description,
       completed,
     };
     setTaskName("");
-    setCompleted(false)
-    setDescription('');
+    setCompleted(false);
+    setDescription("");
     console.log("Task ", task);
+
+    return asyncNewTask(task);
   };
 
   return (
