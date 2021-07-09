b51d7ff68be58d023a01b22453013a67f098ccdb
manjunath00 <kadrimanjunath56@gmail.com>
2021-07-09 08:30:30 +0530

    Revert "Revert "Added state management""
    This reverts commit bd5edd5abe2ff07ccc426d1e3514b3c02730a02a.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import { Navbar } from "reactstrap";
+import "../styles/globals.css";
+import "../components/layout.css";
+import "../styles/dashboard.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head> 
+        <link rel="stylesheet" href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
